ea79a0b92704ab6d025cf8e2edb4fe1344f1e4dd
sinhphamvj <sinhbk07@gmail.com>
2023-09-30 13:14:19 +0700

    window

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,21 +1,25 @@
-handleLogin = (req, res) => {
+import userService from '../services/userService'
+let handleLogin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
+
     if (!email || !password) {
         return res.status(500).json({
-            errorCode: 1,
+            errCode: 1,
             message: 'missing input parameter'
         })
     }
+    let userData = await userService.handleUserLogin(email, password)
+
     return res.status(200).json({
-
-        errorCode: 0,
-
+        errCode: userData.errCode,
+        message: userData.errMessage,
+        user: userData.user ? userData.user : {}
 
     })
 }
 
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
 
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,19 +1,60 @@
 import db from '../models/index'
+import bcrypt from 'bcryptjs'
+let handleUserLogin = (email, password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let userData = {}
+            let isExist = await checkUserEmail(email)
+            console.log('check email 1 >>>>', email)
+            if (isExist) {
+                let user = await db.User.findOne({
+                    attributes: ['email', 'roleId', 'password'],
+                    where: { email: email },
 
-let userHandleLogin = (email, password) => {
+                    raw: true,
+                })
+                if (user) {
 
+                    let check = await bcrypt.compareSync(password, user.password)
+                    console.log('check check 1 >>>>', check)
+                    if (check) {
+                        userData.errCode = 0
+                        userData.errMessage = 'ok'
+                        delete user.password
+                        userData.user = user
+                        console.log('check user 1 >>>>', user)
+
+                    } else {
+                        userData.errCode = 3
+                        userData.errMessage = 'wrong password'
+                        console.log('wrong password')
+                    }
+                } else {
+                    userData.errCode = 2
+                    userData.errMessage = `User's not found`
+                }
+            } else {
+                userData.errCode = 1
+                userData.errMessage = `Your's Email isn't exist in your system. pls try oder email`
+
+            }
+            resolve(userData)
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
 
-let checkUserEmail = (email) => {
+let checkUserEmail = (userEmail) => {
     return new Promise(async (resolve, reject) => {
         try {
             let user = await db.User.findOne({
-                where: { email: userEmail }
+                where: { email: userEmail },
             })
             if (user) {
                 resolve(true)
-            }
-            else {
+                console.log('check email 2 >>>>', email)
+            } else {
                 resolve(false)
             }
         } catch (e) {
@@ -22,6 +63,6 @@ let checkUserEmail = (email) => {
     })
 }
 module.exports = {
-    userHandleLogin: userHandleLogin,
-    checkUserEmail: checkUserEmail,
+    handleUserLogin: handleUserLogin,
+
 }
