22940f6285c7418f2fe48e665564f2ea0d6ecedc
sinhphamvj <sinhbk07@gmail.com>
2023-10-02 16:21:05 +0700

    window

--- src/server.js
+++ src/server.js
@@ -3,10 +3,15 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB';
+const cors = require('cors');
 require('dotenv').config();
 
 let app = express();
 
+app.use(cors({
+    credentials: true,
+    origin: true
+}));
 //config app
 
 app.use(bodyParser.json());
@@ -16,7 +21,8 @@ viewEngine(app);
 initWebRoutes(app);
 connectDB();
 
-let port = process.env.PORT || 6969;
+
+const port = process.env.PORT || 8888;
 //Port === undefined => port = 6969
 
 app.listen(port, () => {
